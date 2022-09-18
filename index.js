90f27e8868ec43bd61ba5fe574ba9927b660423d
Skqlixur <jmjmbo5@gmail.com>
2022-09-18 22:34:53 +0800

    Added cheat mode

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { cheatMode } = require('./config.json')
 const port = process.env.PORT || 2198
 let firstPlayerJoined = false
 app.use(express.json())
@@ -33,6 +34,7 @@ app.post('/player1/:choice', (req, res) => {
   player1Choice = req.params?.choice
 
   res.status(200).json({ choice1: req.params?.choice })
+  if (cheatMode) console.table({ player1Choice, player2Choice })
 })
 
 app.post('/player2/:choice', (req, res) => {
@@ -42,18 +44,24 @@ app.post('/player2/:choice', (req, res) => {
   player2Choice = req?.params?.choice
 
   res.status(200).json({ choice2: req?.params?.choice })
+  if (cheatMode) console.table({ player1Choice, player2Choice })
 })
 
 app.get('/winner', (req, res) => {
   if (!player1Choice || !player2Choice)
     return res.status(404).send('a choice has not yet been selected')
-  let result = evalWinner(player1Choice, player2Choice)
-  result = result === 0 ? 'tie' : result === 1 ? 'player1' : 'player2'
-  res.json({ winner: result }).status(200)
+  res
+    .json({
+      winner: evalWinner(player1Choice, player2Choice),
+      choice1: player1Choice,
+      choice2: player2Choice,
+    })
+    .status(200)
 })
 
 app.put('/resetChoices', (req, res) => {
-  player1Choice = player2Choice = null
+  player1Choice = null
+  player2Choice = null
   res.status(200).send('Choices have been reset.')
 })
 
